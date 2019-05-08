d850bfbc6ae981be3c31c929216c9a7112077635
Morten Andersen <moande15@student.aau.dk>
2019-05-08 22:11:15 +0200

    few changes in employee type

--- Vman_Employee_scraping.R
+++ Vman_Employee_scraping.R
@@ -5,31 +5,105 @@ cat("\014")                                         # Clear console
 if (!require("pacman")) install.packages("pacman")  # Installs/loads pacman
 
 pacman::p_load(rvest)
- 
-# url <- 'https://www.virtualmanager.com/employees/9594140'
+
+# Test ----
+# 
+# url <- 'https://www.virtualmanager.com/employees/10648378'
 # 
 # webpage <- read_html(url)
 # 
 # rank_data_html <- html_nodes(webpage, '.keepers strong , .speciality strong , .job strong , .outfield strong , .youth_players strong')
 # 
-# rank_data <- html_text(rank_data_html)
+# rank_data <- html_text(rank_data_html); rank_data
 
-# min <- 10035549 
-min <- 9595900
-max <- 10676220
-max-min
-counter <- 0
-trainers <- c() #10652691, 10635738, 10675034, 10675137
+# Initialization ----
 
-for(i in min:max){
-  url <- paste('https://www.virtualmanager.com/employees/', i, sep = "")
-  #Sys.sleep(0.1)
-  webpage <- read_html(url)
-  rank_data_html <- html_nodes(webpage, '.keepers strong , .speciality strong , .job strong , .youth_players strong , .outfield strong')
-  rank_data <- html_text(rank_data_html)
-  n <- length(rank_data) # 5 for keepers
-  if(rank_data[1] == "Unemployed" & rank_data[2] == "Trainer" & rank_data[3] == 20 & as.numeric(rank_data[5]) >= 18 & rank_data[n] == 20) {trainers <- c(trainers, paste("TJEK HAM:", i, sep = " "))}
-  counter <- counter + 1
-  print(counter)
-}
+id_min <- 9823626
+id_max <- 9828541
 
+# # min <- 10638444 
+# min <- 10668444 
+# max <- 10676220
+id_max-id_min
+
+empl_type = "Scout"
+#employee_scraping <- function(empl_type = "Outfield trainer", id_min, id_max){
+  
+  counter <- 0
+  
+  if(empl_type == "Outfield trainer"){
+    
+    trainers <- c()
+    
+    for(i in id_min:id_max){
+      url <- paste('https://www.virtualmanager.com/employees/', i, sep = "")
+      #Sys.sleep(0.1)
+      webpage <- read_html(url)
+      rank_data_html <- html_nodes(webpage, '.keepers strong , .speciality strong , .job strong , .youth_players strong , .outfield strong')
+      rank_data <- html_text(rank_data_html)
+      n <- length(rank_data) # 5 for keepers
+      if(rank_data[1] == "Unemployed" & rank_data[2] == "Trainer" & rank_data[3] == 20 & rank_data[n] == 20) {trainers <- c(trainers, paste("OUTFIELD: Check him:", i, sep = " "))}
+      counter <- counter + 1
+      print(counter)
+    }
+    
+    return(trainers)
+    
+  } else if(empl_type == "Keeper trainer"){
+    
+    trainers <- c()
+    
+    for(i in id_min:id_max){
+      url <- paste('https://www.virtualmanager.com/employees/', i, sep = "")
+      #Sys.sleep(0.1)
+      webpage <- read_html(url)
+      rank_data_html <- html_nodes(webpage, '.keepers strong , .speciality strong , .job strong , .youth_players strong , .outfield strong')
+      rank_data <- html_text(rank_data_html)
+      n <- length(rank_data) # 5 for keepers
+      if(rank_data[1] == "Unemployed" & rank_data[2] == "Trainer" & rank_data[3] == 20 & rank_data[5] == 20) {trainers <- c(trainers, paste("KEEPER: Check him:", i, sep = " "))}
+      counter <- counter + 1
+      print(counter)
+    }
+    
+    return(trainers)
+    
+  } else if(empl_type == "Allround trainer"){
+    
+    trainers <- c()
+    
+    for(i in id_min:id_max){
+      url <- paste('https://www.virtualmanager.com/employees/', i, sep = "")
+      #Sys.sleep(0.1)
+      webpage <- read_html(url)
+      rank_data_html <- html_nodes(webpage, '.keepers strong , .speciality strong , .job strong , .youth_players strong , .outfield strong')
+      rank_data <- html_text(rank_data_html)
+      n <- length(rank_data) # 5 for keepers
+      if(rank_data[1] == "Unemployed" & rank_data[2] == "Trainer" & rank_data[3] == 20 & as.numeric(rank_data[5]) >= 19 & rank_data[n] == 20) {trainers <- c(trainers, paste("ALLROUND: Check him:", i, sep = " "))}
+      counter <- counter + 1
+      print(counter)
+    }
+    
+    return(trainers)
+    
+  } else if(empl_type == "Scout"){
+    
+    scouts <- c()}
+  
+  for(i in id_min:id_max){
+    url <- paste('https://www.virtualmanager.com/employees/', i, sep = "")
+    #Sys.sleep(0.1)
+    webpage <- read_html(url)
+    rank_data_html <- html_nodes(webpage, '.potential_assessment strong , .speciality strong , .job strong , .motivation strong , .discipline strong')
+    rank_data <- html_text(rank_data_html)
+    n <- length(rank_data) 
+    if(rank_data[1] == "Unemployed" & rank_data[2] == "Scout" & rank_data[3] == 20 & as.numeric(rank_data[5]) == 20 & rank_data[n] == 20) {scouts <- c(scouts, paste("SCOUT: Check him:", i, sep = " "))}
+    counter <- counter + 1
+    print(counter)
+  }
+  
+  return(scouts)
+  
+# }
+
+
+# employee_scraping(empl_type = "Scout", id_min = min, id_max = max)
